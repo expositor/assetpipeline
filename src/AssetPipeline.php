1c3f07a45737db253e5068345426f29e3680b030
expositor <dan@trendfolio.com>
2015-07-18 17:17:06 -0700

    commit

--- src/AssetPipeline.php
+++ src/AssetPipeline.php
@@ -1,35 +1,40 @@
 <?php namespace Wollnerstudios\AssetPipeline;
-
+//header('Content-Type: application/json');
 require ('../vendor/autoload.php');
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-use Wollnerstudios\AssetPipeline\Assets;
-
 
 class AssetPipeline
 {
-	use Assets;
 
-	protected $assets;
 	protected $baseurl;
+
 	protected $assetPath;
+	
 	protected $fullAssetPath;
+	
 	protected $registeredAssets;
 	
+	protected $addedAssetName;
+	
+	protected $addedCSS;
+	
+	protected $addedJS;
+
 	public function __construct()
 	{
 		
-		$this->registeredAssets = $this->assets();
-		
+		require_once 'Assets.php';
+
+		$this->registeredAssets = $registeredAssets;
+
 		$protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SERVER_PROTOCOL"],'/'))).'://';
 
 		$this->baseurl = $protocol.$_SERVER['HTTP_HOST'].'/';
 
-		//$this->assetPath = 'vendor/wollnerstudios/assetpipeline/src/assets/';
-
-		$this->assetPath = 'src/assets/';
+		$this->assetPath = 'vendor/wollnerstudios/assetpipeline/src/assets/';
 
 		$this->fullAssetPath = $this->baseurl.$this->assetPath;
 
@@ -50,11 +55,150 @@ class AssetPipeline
 		return $this->fullAssetPath = $this->baseurl.$this->assetPath;
 	}
 
-	public function addAsset($asset)
+	public function setAssetPath($assetPath)
 	{
 
-		echo $this->fullAssetPath.$this->registeredAssets['bootstrap']['css'][0];
-		
+		$this->assetPath = $assetPath;
+
+		if (substr($assetPath,-1) !== '/')
+		{
+			$this->assetPath = $assetPath.'/';
+		}
+
+		return $this->fullAssetPath = $this->baseurl.$this->assetPath; 
 	}
+
+	public function addAsset($assetName)
+	{
+
+		$this->addedAssetName = $assetName;
+
+		$this->addedCSS[$this->addedAssetName] = null;
+
+		$this->addedJS[$this->addedAssetName] = null;
+
+		$temporaryAsset = $this->registeredAssets[$assetName];
+		
+		usort($temporaryAsset, function($a,$b){
+
+			return $b['version'] - $a['version'];
+
+		});
+
+		if (isset($temporaryAsset[0]['css']))
+		{
+
+			foreach ($temporaryAsset[0]['css'] as $css)
+			{
+				$this->addedCSS[$this->addedAssetName][] .= '<link href="'.$this->fullAssetPath.''.$css.'" rel="stylesheet">';
+			}
+		}
+
+		if (isset($temporaryAsset[0]['js']))
+		{
+
+			foreach ($temporaryAsset[0]['js'] as $js)
+			{
+				$this->addedJS[$this->addedAssetName][] .= '<script src="'.$this->fullAssetPath.''.$js.'"></script>';
+			}
+		}
+
+		return $this;
+	}
+
+	public function version($versionNumber)
+	{
+		$temporaryAsset = $this->registeredAssets[$this->addedAssetName];
+
+		$key = array_search($versionNumber, array_column($temporaryAsset,'version'));
+		
+		$this->addedCSS[$this->addedAssetName] = null;
+
+		$this->addedJS[$this->addedAssetName] = null;
+
+		if (isset($temporaryAsset[$key]['css']))
+		{
+
+			foreach ($temporaryAsset[$key]['css'] as $css)
+			{
+
+				$this->addedCSS[$this->addedAssetName][] .= '<link href="'.$this->fullAssetPath.''.$css.'" rel="stylesheet">';
+			}
+
+		}
+
+		if (isset($temporaryAsset[$key]['js']))
+		{
+
+			foreach ($temporaryAsset[$key]['js'] as $js)
+			{
+
+				$this->addedJS[$this->addedAssetName][] .= '<script src="'.$this->fullAssetPath.''.$js.'"></script>';
+			}
+
+		}
+
+		return $this;
+
+	}
+	
+
+	public function withoutBaseURL()
+	{
+		
+		if (isset($this->addedCSS[$this->addedAssetName]))
+		{
+			foreach ($this->addedCSS[$this->addedAssetName] as $key=>$value)
+			{
+				$this->addedCSS[$this->addedAssetName][$key] = preg_replace("|".preg_quote($this->fullAssetPath)."|",null,$value,1);
+			}
+		}
+
+		if (isset($this->addedJS[$this->addedAssetName]))
+		{
+			foreach ($this->addedJS[$this->addedAssetName] as $key=>$value)
+			{
+				$this->addedJS[$this->addedAssetName][$key] = preg_replace("|".preg_quote($this->fullAssetPath)."|",null,$value,1);
+			}
+		}
+
+		return;
+	}
+
+	public function showCSS()
+	{
+
+		foreach ($this->addedCSS as $css)
+		{
+			
+			if (isset($css))
+			{
+			
+				foreach($css as $css)
+				{
+					echo $css."\r\n";
+				}				
+			}
+		}
+
+		return;
+	}
+
+	public function showJS()
+	{
+
+		foreach ($this->addedJS as $js)
+		{
+			if (isset($js))
+			{
+				foreach($js as $js)
+				{
+					echo $js."\r\n";
+				}
+			}
+		}
+		return;
+	}
+
 
 }
--- src/Assets.php
+++ src/Assets.php
@@ -1,17 +1,40 @@
-<?php namespace Wollnerstudios\AssetPipeline;
+<?php 
 
-trait Assets {
-	function assets()
-	{
-		$assets = 
+$registeredAssets = 
+[
+
+	'bootstrap' => 
+	[
 		[
-			'bootstrap' => [
-				'css' => ['bootstrap-3.3.5/css/bootstrap.css'],
-				'js' => ['bootstrap.js']
-			],
-			
+			'version' => '3.3.5',
+			'css'=>['bootstrap-3.3.5/css/bootstrap.css'],
+			'js'=>['bootstrap-3.3.5/js/bootstrap.js'],
+			'cssMin' => ['bootstrap-3.3.5/css/bootstrap.min.css'],
+			'jsMin' => ['bootstrap-3.3.5/js/bootstrap.min.js'],
+		],
 
-		];
-		return $assets;
-	}
-}
+	],
+
+	'jQuery' => 
+	[
+		[
+			'version' => '1.11.3',
+			'js'=>['https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'],
+			'jsMin' => ['jquery.min.js'],
+		],
+		[
+			'version' => '2.1.4',
+			'js'=>['https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js'],
+			'jsMin' => ['jquery.min.js'],
+		]		
+	],
+	'Open Sans' => 
+	[
+		[
+			'version' => '1.0.0',
+			'css'=>['http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'],
+		]
+	],
+
+
+];
